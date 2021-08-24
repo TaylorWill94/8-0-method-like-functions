aca5a016e9d4feb6ecbe01d97b2a648b69ee1718
Taylor W <taylorwilliams@pursuit.org>
2021-08-24 10:27:17 -0400

    Some problems completed.

--- index.js
+++ index.js
@@ -4,14 +4,26 @@
  * @returns {number} the new length of the array.
  */
 
-function myPushFunction() {}
+function myPushFunction(array, element) {
+  array[array.length] = element;
+  return array.length;
+}
 
 /**
  * Removes the last element of the array and returns the removed element or undefined if the array is empty. Do not use the pop method.
  * @param {Array[]} array - an array of with any kind of elements.
  * @returns {number} the new length of the array .
  */
-function myPopFunction() {}
+function myPopFunction(array) {
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  let removedElement = array[array.length - 1];
+  array.length = array.length - 1;
+
+  return removedElement;
+}
 
 /**
  * Determine whether an array includes a specific value within its entries. Do not use the includes method.
@@ -19,24 +31,66 @@ function myPopFunction() {}
  * @param {*} searchElement - an element of any type that you are searching for in the array.
  * @returns {boolean} returns true or false if the searchElement is found in the array.
  */
-function myIncludesFunction() {}
+function myIncludesFunction(array, searchElement) {
+  //let found = false;
+  for (const arr of array) {
+    if (arr === searchElement) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /**
  * Return the first index in an array where a specified value is found. Do not use the indexOf method.
  * @param {Array[*]} array - an array of with any kind of elements.
  * @param {*} searchElement - an element of any type that you are searching for in the array.
- * @returns {number} returns the index at with the searchElement is found or -1.
+ * @returns {number} returns the index at which the searchElement is found or -1.
  */
-function myIndexOfFunction() {}
+function myIndexOfFunction(array, searchElement) {
+  for (let i = 0; i < array.length; i++) {
+    const arr = array[i];
+    if (arr === searchElement) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 /**
- * Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified. Do not use the slice method.
+ * Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included)
+ * where start and end represent the index of items in that array. The original array will not be modified. Do not use the slice method.
  * @param {Array[*]} array - an array of with any kind of elements.
  * @param {number} [startIdx] - an optional number representing the starting index of the extraction. The startIdx can be negative.
  * @param {number} [endIdx] - an optional number representing the ending index of the extraction (non-inclusive). The endIdx can be negative.
  * @returns {Array[*]} returns a new array containing the extracted elements.
  */
-function mySliceFunction() {}
+function mySliceFunction(array, startIdx, endIdx) {
+  let newArr = [];
+
+  if (!startIdx || !endIdx) {
+    newArr = array;
+    return newArr;
+  }
+
+  if (startIdx || endIdx) {
+    for (let i = 0; i < array.length; i++) {
+      //i = startIdx;
+      newArr = array;
+      return newArr;
+    }
+  }
+  let i = startIdx;
+  while (i < endIdx) {
+    newArr[i] = array[i];
+    i++;
+  }
+  return newArr;
+}
+
+//if (startIdx && endIdx) {
+//return newArr;
+//}
 
 /**
  * Creates and returns a new string by concatenating all of the elements in an array, separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator. Do not use the join method.
@@ -61,8 +115,6 @@ function myReverseFunction() {}
  */
 function myUnshiftFunction() {}
 
-
-
 module.exports = {
   myPushFunction,
   myPopFunction,
@@ -71,5 +123,5 @@ module.exports = {
   mySliceFunction,
   myJoinFunction,
   myReverseFunction,
-  myUnshiftFunction
+  myUnshiftFunction,
 };
